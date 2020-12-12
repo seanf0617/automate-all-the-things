a728605ecad03dfa1a52b7fbaa9a75ab3fc02114
chris <chris.gallivan@fcagroup.com>
2020-12-12 10:48:04 -0500

    fixed misspelled word timestamp :)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express= require ("express");
 var app = express();
 
 app.get("/url",(req,res,next) => {
-res.json({"message":"Automate all the things!","timstamp":"1529729125"});
+res.json({"message":"Automate all the things!","timestamp":"1529729125"});
 });
 
 //app.listen (3000, () => {
