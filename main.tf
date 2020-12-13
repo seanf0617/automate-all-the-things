9f2f2b75bbe57ff656c039cd159aad34ed743cf3
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2020-12-13 16:40:13 -0500

    added s3 bucket and dynamo table for locks

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
 provider "aws" {
   region="us-east-2"
 }
+
+resource "aws_s3_bucket" "terraform_state"{
+    bucket = "automate-all-the-things-terraform-state"
+    
+    versioning {
+      enabled = true  
+    }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamo_table" "terraform_state_locks" {
+  name = "automate-all-the-things-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
