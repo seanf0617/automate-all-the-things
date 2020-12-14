16f10e54fdd8c77bf8f9fb107e28beebea5c2311
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2020-12-13 22:54:10 -0500

    Delete terraformConfig.tf

--- terraformConfig.tf
+++
@@ -1,5 +0,0 @@
-   bucket = "automate-all-the-things-terraform-state"
-   key = "global/s3/terraform.tfstate"
-   region = "us-east-2"
-   dynamodb_table = "automate-all-the-things-terraform-locks"
-   encrypt = true
