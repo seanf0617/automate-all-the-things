0c959efc221f5593efdeedff03af583e3298bcc1
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2020-12-14 21:54:22 -0500

    Create terraformConfig.tf

---
+++ terraformConfig.tf
@@ -0,0 +1,9 @@
+terraform {
+ backend "s3" {
+   bucket = "automate-all-the-things-terraform-state"
+   key = "global/s3/terraform.tfstate"
+   region = "us-east-2"
+   dynamodb_table = "automate-all-the-things-terraform-locks"
+   encrypt = true
+   }
+ }
